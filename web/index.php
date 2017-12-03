c08bf4456781d146f9f17ccea81f2a1282377c94
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-12-03 14:28:45 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
   <body> 
     <?php 
         echo ("Ecco tutte le pizzerie di bergamo:");
-        $r = new HttpRequest('Http:://google.it?bose', HttpRequest::METH_POST);
+        $r = new HttpRequest('Http:://google.it//search?source=hp&ei=7PsjWr_OFtOiUvOjjPgJ&q=bose&oq=bose&gs_l=psy-ab.3..0i131k1j0j0i131k1l2j0l6.3164.3989.0.4387.7.4.0.0.0.0.135.489.0j4.4.0....0...1c.1.64.psy-ab..3.4.487.0...0.M_dAQ6kpKlo', HttpRequest::METH_POST);
       // $r->setOptions(array('lastmodified' => filemtime('local.rss')));
        // $r->addQueryData(array('category' => 3));
         try {
