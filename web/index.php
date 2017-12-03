212d1178f3e765356b0cbf8d209022d9712a3647
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-12-03 14:20:39 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
    </head> 
   <body> 
     <?php 
+        echo (<p>"Ecco tutte le pizzerie di bergamo:"</p>)
         $r = new HttpRequest('api.foursquare.com/v2/venues/49d51ce3f964a520675c1fe3?v=20171203&client_id=3AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W&client_secret=NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV
 ', HttpRequest::METH_POST);
        // $r->setOptions(array('cookies' => array('lang' => 'de')));
