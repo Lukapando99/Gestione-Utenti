793c2cddd5a1e72bb1573798d3bf2ef43654a05b
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-12-03 14:19:14 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,21 +1,21 @@
 <html> 
     <head> 
-        <title>Contatore accessi</title> 
+        <title>Cerca pizzerie</title> 
    </head> 
   <body> 
-<?php 
-    if (!isset($_COOKIE['count']))
-    {
-        echo "Benvenuto! Questa e' la prima volta che accedi a questo sito!";
-        $cookie = 1;
-        setcookie("count", $cookie,time()+60*60*24*7);
-    }
-    else
-    {
-        $cookie = $_COOKIE['count']+1;
-        echo "Hai visitato questa pagina $cookie volte."; 
-        setcookie("count", $cookie);
-    }  
+    <?php 
+        $r = new HttpRequest('api.foursquare.com/v2/venues/49d51ce3f964a520675c1fe3?v=20171203&client_id=3AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W&client_secret=NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV
+', HttpRequest::METH_POST);
+       // $r->setOptions(array('cookies' => array('lang' => 'de')));
+       // $r->addPostFields(array('user' => 'mike', 'pass' => 's3c|r3t'));
+       // $r->addPostFile('image', 'profile.jpg', 'image/jpeg');
+        try {
+            echo $r->send()->getBody();
+        } catch (HttpException $ex) {
+            echo $ex;
+        }
+        
+ 
       ?> 
    </body> 
 </html>
