fa1266c54ec685f66b755c46c46b6cc99b476422
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-12-03 14:57:56 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,29 +1,45 @@
-<html> 
-    <head> 
-        <title>Cerca pizzerie</title> 
-   </head> 
-  <body> 
-    <?php 
-        echo ("Ecco tutte le pizzerie di bergamo:");
-        $r = new HttpRequest('Http:://google.it//search?source=hp&ei=7PsjWr_OFtOiUvOjjPgJ&q=bose&oq=bose&gs_l=psy-ab.3..0i131k1j0j0i131k1l2j0l6.3164.3989.0.4387.7.4.0.0.0.0.135.489.0j4.4.0....0...1c.1.64.psy-ab..3.4.487.0...0.M_dAQ6kpKlo', HttpRequest::METH_POST);
-      // $r->setOptions(array('lastmodified' => filemtime('local.rss')));
-       // $r->addQueryData(array('category' => 3));
-        try {
-            $r->send();
-            if ($r->getResponseCode() == 200) {
-                file_put_contents('local.rss', $r->getResponseBody());
-            }
-        } catch (HttpException $ex) {
-            echo $ex;
-        }
-        
- 
-      ?> 
-   </body> 
+<?php 
+	require_once("../src/FoursquareApi.class.php");
+	
+	// This file is intended to be used as your redirect_uri for the client on Foursquare
+	
+	// Set your client key and secret
+	$client_key = "3AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W";
+	$client_secret = "NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV";
+	$redirect_uri = "20170101";
+	
+	// Load the Foursquare API library
+	$foursquare = new FoursquareApi($client_key,$client_secret);
+	
+	// If the link has been clicked, and we have a supplied code, use it to request a token
+	if(array_key_exists("code",$_GET)){
+		$token = $foursquare->GetToken($_GET['code'],$redirect_uri);
+	}
+	
+?>
+<!doctype html>
+<html>
+<head>
+	<title>PHP-Foursquare :: Token Request Example</title>
+</head>
+<body>
+<h1>Token Request Example</h1>
+<p>
+	<?php 
+	// If we have not received a token, display the link for Foursquare webauth
+	if(!isset($token)){ 
+		echo "<a href='".$foursquare->AuthenticationLink($redirect_uri)."'>Connect to this app via Foursquare</a>";
+	// Otherwise display the token
+	}else{
+		echo "Your auth token: $token";
+	}
+	
+	?>
+	
+</p>
+<hr />
+<?php 
+	
+?>
+</body>
 </html>
-
-
-
-
-
-
