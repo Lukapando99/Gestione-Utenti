2f03ad6dcb78f1c2cf2b1f407c8af96ae9f1ef28
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-12-05 16:57:34 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
 $r = new HttpRequest('https://api.foursquare.com/v2/venues/explore', HttpRequest::METH_GET);
 //$r->setOptions(array('lastmodified' => filemtime('local.rss')));
-$r->addQueryData(array('client_id' => AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W));
-$r->addQueryData(array('client_secret' => NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV));
-$r->addQueryData(array('v' => 20170801 ));
+$r->addQueryData(array('client_id' => "AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W"));
+$r->addQueryData(array('client_secret' => "NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV"));
+$r->addQueryData(array('v' => "20170801" ));
 $r->addQueryData(array('query' => "pizza"));
 
 try {
